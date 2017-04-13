62436cdec73864ffc1a56036b1fbfe9564b4a83d
kephin <wunshon76325@gmail.com>
2017-04-13 23:59:57 +0800

    add route for Post to /api/drivers

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,5 +1,12 @@
+const Driver = require('../models/drivers');
+
 module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' });
+  },
+  async create(req, res) {
+    const driverProps = req.body;
+    const driver = await Driver.create(driverProps);
+    res.send(driver);
   },
 };
