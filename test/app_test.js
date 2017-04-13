35510fe341915578387a8bf2c639b62ce8c357fa
kephin <wunshon76325@gmail.com>
2017-04-13 23:55:40 +0800

    refactoring to ES7 async/await syntax

--- test/app_test.js
+++ test/app_test.js
@@ -4,12 +4,8 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('The express app', () => {
-  it('handles a GET request to /api', (done) => {
-    request(app)
-      .get('/api')
-      .end((err, res) => {
-        assert(res.body.hi === 'there');
-        done();
-      });
+  it('handles a GET request to /api', async() => {
+    const res = await request(app).get('/api');
+    assert(res.body.hi === 'there');
   });
 });
