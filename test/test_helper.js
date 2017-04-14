15077d8c210cd347551b690afc6c95cbbf07da24
kephin <wunshon76325@gmail.com>
2017-04-14 22:13:05 +0800

    set test env and create test database

---
+++ test/test_helper.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+before(done => {
+  mongoose.connect('mongodb://localhost/muber_test');
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', err => console.warn('Warning', err));
+});
