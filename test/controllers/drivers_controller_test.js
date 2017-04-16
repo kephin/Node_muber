a982f774768191a4d033547c7729cc0c93918b45
kephin <wunshon76325@gmail.com>
2017-04-16 13:33:15 +0800

    add test for validation of Post

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -7,18 +7,19 @@ const Driver = mongoose.model('Driver');
 
 describe('Drivers controller', () => {
   it('Post to /api/drivers creates a new driver', async() => {
-    try {
-      const beforeCount = await Driver.count();
-      const res = await request(app)
-        .post('/api/drivers')
-        .send({
-          name: 'kevin',
-          email: 'kevin@test.com',
-        });
-      const newCount = await Driver.count();
-      assert(beforeCount + 1 === newCount);
-    } catch (err) {
-      console.log(err);
-    }
+    await request(app)
+      .post('/api/drivers')
+      .send({
+        name: 'kevin',
+        email: 'kevin@test.com',
+      });
+    const count = await Driver.count();
+    assert(count === 1);
+  });
+  it('Post to /api/drivers requires name and email', async() => {
+    const res = await request(app)
+      .post('/api/drivers')
+      .send({});
+    assert(res.error);
   });
 });
