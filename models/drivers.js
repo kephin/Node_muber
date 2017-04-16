811ad8b625648d000e1b380e74aad0c730769690
kephin <wunshon76325@gmail.com>
2017-04-17 01:27:25 +0800

    add geojson schema to the driver model

--- models/drivers.js
+++ models/drivers.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const pointSchema = new Schema({
+  type: {
+    type: String,
+    default: 'Point',
+  },
+  coordinates: {
+    type: [Number],
+    index: '2dsphere',
+  },
+});
+
 const driverSchema = new Schema({
   name: {
     type: String,
@@ -14,6 +25,7 @@ const driverSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  geometry: pointSchema,
 });
 
 const Driver = mongoose.model('Driver', driverSchema);
